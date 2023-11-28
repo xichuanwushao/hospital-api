e6045a715310032735afc41c8c6e6e6c1e4b743f
xichuanwushao <avatarwx@163.com>
2023-11-28 12:32:27 +0800

    Service层login方法实现

---
+++ src/main/java/com/example/hospital/api/service/MisUserService.java
@@ -0,0 +1,13 @@
+package com.example.hospital.api.service;
+
+import java.util.Map;
+
+/**
+ * @author : wuxiao
+ * @date : 12:26 2023-11-28
+ */
+public interface MisUserService {
+
+    public Integer login(Map param);
+
+}
