700a88c475f171c821f8d8677426d45aa27205d1
xichuanwushao <avatarwx@163.com>
2023-12-06 15:58:36 +0800

    医疗科室模块控制层

--- src/main/java/com/example/hospital/api/controller/MedicalDeptController.java
+++ src/main/java/com/example/hospital/api/controller/MedicalDeptController.java
@@ -1,15 +1,20 @@
 package com.example.hospital.api.controller;
 
 import cn.dev33.satoken.annotation.SaCheckLogin;
+import cn.dev33.satoken.annotation.SaCheckPermission;
+import cn.dev33.satoken.annotation.SaMode;
+import cn.hutool.core.bean.BeanUtil;
+import com.example.hospital.api.common.PageUtils;
 import com.example.hospital.api.common.R;
+import com.example.hospital.api.controller.form.SearchMedicalDeptByPageForm;
 import com.example.hospital.api.service.MedicalDeptService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author : wuxiao
@@ -34,5 +39,17 @@ public class MedicalDeptController {
         return R.ok().put("result", map);
     }
 
+    @PostMapping("/searchByPage")
+    @SaCheckLogin
+    @SaCheckPermission(value = {"ROOT", "MEDICAL_DEPT:SELECT"}, mode = SaMode.OR)
+    public R searchByPage(@RequestBody @Valid SearchMedicalDeptByPageForm form) {
+        Map param = BeanUtil.beanToMap(form);
+        int page = form.getPage();
+        int length = form.getLength();
+        int start = (page - 1) * length;
+        param.put("start", start);
+        PageUtils pageUtils = medicalDeptService.searchByPage(param);
+        return R.ok().put("result", pageUtils);
+    }
 }
 
