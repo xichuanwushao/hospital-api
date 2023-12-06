7e5b24cdf7ba71ddabdbeed2a8d748d9144b0c94
xichuanwushao <avatarwx@163.com>
2023-12-06 15:58:08 +0800

    医疗科室模块持久层, 分别统计每个科室的诊室数量和医生数量

--- src/main/java/com/example/hospital/api/db/dao/MedicalDeptDao.java
+++ src/main/java/com/example/hospital/api/db/dao/MedicalDeptDao.java
@@ -2,12 +2,16 @@ package com.example.hospital.api.db.dao;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public interface MedicalDeptDao {
     public ArrayList<HashMap> searchAll();
 
     public ArrayList<HashMap> searchDeptAndSub();
 
+    public ArrayList<HashMap> searchByPage(Map param);
+
+    public long searchCount(Map param);
 
 }
 
