66786e4567e4f8e9604215ecc80f2f924be792b6
xichuanwushao <avatarwx@163.com>
2023-12-07 16:39:57 +0800

    添加诊室记录持久层

--- src/main/java/com/example/hospital/api/db/dao/MedicalDeptSubDao.java
+++ src/main/java/com/example/hospital/api/db/dao/MedicalDeptSubDao.java
@@ -1,5 +1,7 @@
 package com.example.hospital.api.db.dao;
 
+import com.example.hospital.api.db.pojo.MedicalDeptSubEntity;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -7,6 +9,9 @@ import java.util.Map;
 public interface MedicalDeptSubDao {
     public ArrayList<HashMap> searchByPage(Map param);
     public long searchCount(Map param);
+
+    public void insert(MedicalDeptSubEntity entity);
+
 }
 
 
