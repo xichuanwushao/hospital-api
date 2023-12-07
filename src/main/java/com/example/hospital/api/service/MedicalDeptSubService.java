4c8f69b365d8b54dfa5cad89fa5470fccc2ea1d6
xichuanwushao <avatarwx@163.com>
2023-12-07 16:08:48 +0800

    诊室分页记录业务层

---
+++ src/main/java/com/example/hospital/api/service/MedicalDeptSubService.java
@@ -0,0 +1,14 @@
+package com.example.hospital.api.service;
+
+import com.example.hospital.api.common.PageUtils;
+
+import java.util.Map;
+
+/**
+ * @author : wuxiao
+ * @date : 16:01 2023-12-07
+ */
+public interface MedicalDeptSubService {
+    public PageUtils searchByPage(Map param);
+
+}
---
+++ src/main/java/com/example/hospital/api/service/impl/MedicalDeptSubServiceImpl.java
@@ -0,0 +1,39 @@
+package com.example.hospital.api.service.impl;
+
+import cn.hutool.core.map.MapUtil;
+import com.example.hospital.api.common.PageUtils;
+import com.example.hospital.api.db.dao.MedicalDeptSubDao;
+import com.example.hospital.api.service.MedicalDeptSubService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author : wuxiao
+ * @date : 16:01 2023-12-07
+ */
+@Service
+public class MedicalDeptSubServiceImpl implements MedicalDeptSubService {
+
+
+    @Resource
+    private MedicalDeptSubDao medicalDeptSubDao;
+    @Override
+    public PageUtils searchByPage(Map param) {
+        ArrayList<HashMap> list = null;
+        long count = medicalDeptSubDao.searchCount(param);
+        if (count > 0) {
+            list = medicalDeptSubDao.searchByPage(param);
+        } else {
+            list = new ArrayList<>();
+        }
+        int page = MapUtil.getInt(param, "page");
+        int length = MapUtil.getInt(param, "length");
+        PageUtils pageUtils = new PageUtils(list, count, page, length);
+        return pageUtils;
+    }
+
+}
