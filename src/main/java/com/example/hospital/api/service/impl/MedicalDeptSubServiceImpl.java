bf6a10226615b1a4d5b2bee06084acfd4a512363
xichuanwushao <avatarwx@163.com>
2023-12-14 15:47:23 +0800

    封装查询出诊计划数服务层代码编写

--- src/main/java/com/example/hospital/api/service/impl/MedicalDeptSubServiceImpl.java
+++ src/main/java/com/example/hospital/api/service/impl/MedicalDeptSubServiceImpl.java
@@ -1,17 +1,21 @@
 package com.example.hospital.api.service.impl;
 
 import cn.hutool.core.map.MapUtil;
+import cn.hutool.json.JSONArray;
 import com.example.hospital.api.common.PageUtils;
+import com.example.hospital.api.db.dao.DoctorWorkPlanDao;
 import com.example.hospital.api.db.dao.MedicalDeptSubDao;
 import com.example.hospital.api.db.pojo.MedicalDeptSubEntity;
 import com.example.hospital.api.exception.HospitalException;
 import com.example.hospital.api.service.MedicalDeptSubService;
+import com.example.hospital.api.service.MedicalDeptSubWorkPlanService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
@@ -24,6 +28,9 @@ public class MedicalDeptSubServiceImpl implements MedicalDeptSubService {
 
     @Resource
     private MedicalDeptSubDao medicalDeptSubDao;
+
+    private DoctorWorkPlanDao doctorWorkPlanDao;
+
     @Override
     public PageUtils searchByPage(Map param) {
         ArrayList<HashMap> list = null;
@@ -67,4 +74,68 @@ public class MedicalDeptSubServiceImpl implements MedicalDeptSubService {
         }
     }
 
+
+
+    public JSONArray searchWorkPlanInRange(Map param, ArrayList dateList) {
+        ArrayList<HashMap> list = doctorWorkPlanDao.searchWorkPlanInRange(param);
+        Integer tempSubId = null;
+        String tempDate = null;
+        HashMap tempResult = new HashMap();
+        for (HashMap one : list) {
+            String deptName = MapUtil.getStr(one, "deptName");
+            int deptSubId = MapUtil.getInt(one, "deptSubId");
+            String deptSubName = MapUtil.getStr(one, "deptSubName");
+            String doctorName = MapUtil.getStr(one, "doctorName");
+            int workPlanId = MapUtil.getInt(one, "workPlanId");
+            String date = MapUtil.getStr(one, "date");
+
+            //判断是不是第一条记录
+            if (tempSubId == null) {
+                tempSubId = deptSubId;
+                tempDate = date;
+                //把第一条记录当做一个诊室
+                HashMap temp = new HashMap() {{
+                    put("deptName", deptName);
+                    put("deptSubId", deptSubId);
+                    put("deptSubName", deptSubName);
+                    /*
+                     * 该诊室出诊计划
+                     * 为了保证添加的顺序不被打乱，必须用LinkedHashMap，不可以是HashMap
+                     */
+                    put("plan", new LinkedHashMap<>() {{
+                        put(date, new ArrayList<>() {{
+                            add(doctorName);
+                        }});
+                    }});
+                }};
+                tempResult.put(deptSubId, temp);
+            }
+            //非第一条记录，但是该记录与前一条记录是相同诊室，而且是同一天出诊
+            else if (tempSubId == deptSubId && tempDate.equals(date)) {
+                //取出该诊室
+                HashMap map = (HashMap) tempResult.get(deptSubId);
+                //从诊室中取出出诊计划
+                LinkedHashMap plan = (LinkedHashMap) map.get("plan");
+                //找到该天出诊医生名单列表
+                ArrayList doctors = (ArrayList) plan.get(date);
+                //把医生名字添加到列表中
+                doctors.add(doctorName);
+            }
+        //非第一条记录，但是该记录与前一条记录是相同诊室，但不是同一天出诊
+            else if (tempSubId == deptSubId && !tempDate.equals(date)){
+                tempDate = date; //更新日期
+                //取出该诊室
+                HashMap map = (HashMap) tempResult.get(deptSubId);
+                //从诊室中取出出诊计划
+                LinkedHashMap plan = (LinkedHashMap) map.get("plan");
+                //创建新的出诊日期列表，添加该医生的名字
+                plan.put(date, new ArrayList<>() {{
+                    add(doctorName);
+                }});
+            }
+
+        }
+
+        return null;
+    }
 }
