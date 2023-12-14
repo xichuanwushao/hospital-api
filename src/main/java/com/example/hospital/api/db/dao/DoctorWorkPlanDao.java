f746656521c76b087bf1b3fb58849bae1a7a6c05
xichuanwushao <avatarwx@163.com>
2023-12-14 15:46:46 +0800

    封装查询出诊计划数持久层

--- src/main/java/com/example/hospital/api/db/dao/DoctorWorkPlanDao.java
+++ src/main/java/com/example/hospital/api/db/dao/DoctorWorkPlanDao.java
@@ -1,8 +1,12 @@
 package com.example.hospital.api.db.dao;
 
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public interface DoctorWorkPlanDao {
-    
+    public ArrayList<HashMap> searchWorkPlanInRange(Map param);
 }
 
 
