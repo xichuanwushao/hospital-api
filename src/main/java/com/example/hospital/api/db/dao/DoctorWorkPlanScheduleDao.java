be19bf6e08d716066b021300107ff91812a30de2
xichuanwushao <avatarwx@163.com>
2023-12-18 13:45:05 +0800

    封装查询医生日程接口 持久层代码

--- src/main/java/com/example/hospital/api/db/dao/DoctorWorkPlanScheduleDao.java
+++ src/main/java/com/example/hospital/api/db/dao/DoctorWorkPlanScheduleDao.java
@@ -4,10 +4,16 @@ import com.example.hospital.api.db.pojo.DoctorWorkPlanScheduleEntity;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public interface DoctorWorkPlanScheduleDao {
     public void insert(DoctorWorkPlanScheduleEntity entity);
     public ArrayList<HashMap> searchNewSchedule(int workPlanId);
+
+    public ArrayList<HashMap> searchDeptSubSchedule(Map param);
+
+
+
 }
 
 
